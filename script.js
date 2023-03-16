493e10cab7d33fdddbc22f6999fa12c17adb91ae
Alessandro Trupo <atr00@icloud.com>
2023-03-16 22:05:12 +0800

    Implemented the game logic

---
+++ script.js
@@ -0,0 +1,52 @@
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerSelection () {
+    const computerSelection = choices[Math.floor(Math.random() * 3)];
+    return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === "rock") {
+        if (playerSelection === "rock") {
+            return "Tie! Rock against Rock!";
+        } else if (playerSelection === "paper") {
+            return "You win! Paper beats Rock!";
+        } else if (playerSelection === "scissors") {
+            return "You lose! Rock beats paper!";
+        } else {
+            return "You did not enter a correct choice."
+        }
+    } else if (computerSelection === "paper") {
+        if (playerSelection === "paper") {
+            return "Tie! Paper against Paper!";
+        } else if (playerSelection === "rock") {
+            return "You lose! Paper beats Rock!";
+        } else if (playerSelection === "scissors") {
+            return "You win! Scissors beats paper!";
+        } else {
+            return "You did not enter a correct choice."
+        }
+    } else if (computerSelection === "scissors") {
+        if (playerSelection === "scissors") {
+            return "Tie! Scissors against Scissors!";
+        } else if (playerSelection === "rock") {
+            return "You win! Rock beats scissors!";
+        } else if (playerSelection === "paper") {
+            return "You lose! Scissors beats paper!";
+        } else {
+            return "You did not enter a correct choice."
+        }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Make a choice (rock, paper, scissors): ").toLowerCase();
+        result = playRound(playerSelection, getComputerSelection())
+        console.log(result);
+    }
+}
+
+game();
+
+
